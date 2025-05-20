0cbffe0058423be6761b89bb018cb5d4a463d61d
123485k <1758961307@qq.com>
2025-05-20 19:19:56 +0800

    refactor(src): 将 Modbus 寄存器访问实现移动到新模块中
    - 将 ModbusRegisterAccess  trait 的实现从 main.rs 移动到新创建的 modbus_access.rs 文件中
    - 在 main.rs 中引入新的 modbus_access 模块
    - 优化代码结构，提高可维护性和可读性

--- src/main.rs
+++ src/main.rs
@@ -15,33 +15,7 @@ use modbus::{ModbusRegisterAccess, ModbusService};
 mod model;
 use model::{ModleHandler};
 
-impl ModbusRegisterAccess for ModleHandler {
-    fn read_input_regs(&self, _addr: u16, _cnt: u16) -> Result<Vec<u16>, ExceptionCode> {
-        let regs = self.input_registers.lock().unwrap();
-        if _addr + _cnt > regs.len() as u16 {
-            return Err(ExceptionCode::IllegalDataAddress);
-        }
-        Ok(regs[_addr as usize..(_addr + _cnt) as usize].to_vec())
-    }
-
-    fn read_holding_regs(&self, _addr: u16, _cnt: u16) -> Result<Vec<u16>, ExceptionCode> {
-        let regs = self.input_registers.lock().unwrap();
-        if _addr + _cnt > regs.len() as u16 {
-            return Err(ExceptionCode::IllegalDataAddress);
-        }
-        Ok(regs[_addr as usize..(_addr + _cnt) as usize].to_vec())
-    }
-
-    fn write_regs(&mut self, _addr: u16, _values: &[u16]) -> Result<(), ExceptionCode> {
-        println!("{:?}", _values);
-        let mut hold_regs = self.holding_registers.lock().unwrap();
-        if _addr as usize + _values.len() > hold_regs.len() {
-            return Err(ExceptionCode::IllegalDataAddress);
-        }
-        hold_regs[_addr as usize..(_addr as usize + _values.len())].copy_from_slice(_values);
-        Ok(())
-    }
-}
+mod modbus_access;
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
---
+++ src/modbus_access.rs
@@ -0,0 +1,31 @@
+use crate::model::ModleHandler;
+use crate::modbus::ModbusRegisterAccess;
+use tokio_modbus::prelude::ExceptionCode;
+
+impl ModbusRegisterAccess for ModleHandler {
+    fn read_input_regs(&self, _addr: u16, _cnt: u16) -> Result<Vec<u16>, ExceptionCode> {
+        let regs = self.input_registers.lock().unwrap();
+        if _addr + _cnt > regs.len() as u16 {
+            return Err(ExceptionCode::IllegalDataAddress);
+        }
+        Ok(regs[_addr as usize..(_addr + _cnt) as usize].to_vec())
+    }
+
+    fn read_holding_regs(&self, _addr: u16, _cnt: u16) -> Result<Vec<u16>, ExceptionCode> {
+        let regs = self.holding_registers.lock().unwrap();
+        if _addr + _cnt > regs.len() as u16 {
+            return Err(ExceptionCode::IllegalDataAddress);
+        }
+        Ok(regs[_addr as usize..(_addr + _cnt) as usize].to_vec())
+    }
+
+    fn write_regs(&mut self, _addr: u16, _values: &[u16]) -> Result<(), ExceptionCode> {
+        println!("{:?}", _values);
+        let mut hold_regs = self.holding_registers.lock().unwrap();
+        if _addr as usize + _values.len() > hold_regs.len() {
+            return Err(ExceptionCode::IllegalDataAddress);
+        }
+        hold_regs[_addr as usize..(_addr as usize + _values.len())].copy_from_slice(_values);
+        Ok(())
+    }
+}
